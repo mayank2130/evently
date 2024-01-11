0e415f2550b0ec6fc95cad11a464d964a2d725bd
mayank2130 <mayankthakur1712@gmail.com>
2024-01-11 19:52:20 +0530

    mongodb setup and creating user.model and event.model

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="border-t w-full fixed bottom-0">
+    <footer className="border-b ">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href="/">
           <Image
